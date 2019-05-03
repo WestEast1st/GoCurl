2a36351ed049e45e41741af3175d27f285791615
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-04 02:13:17 +0900

    HTTP関連の情報をまとめる

---
+++ infomation/infomation.go
@@ -0,0 +1,28 @@
+package infomation
+
+import "net/url"
+
+// 送信するHTTP関連の情報
+type HttpInfomation struct {
+	URL      string
+	URI      string
+	Method   string
+	Port     string
+	Hostname string
+	Path     string
+	Query    url.Values
+	Data     url.Values
+	Fragment string
+	Output   Output
+	Header   Header
+}
+
+// アウトプットの関連情報
+type Output struct {
+	Flag     bool
+	Filepath string //ユーザーが配置したいディレクトリ
+	Filename string //命名のPATH
+}
+
+type Header struct {
+}
