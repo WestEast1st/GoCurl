15b674ecd2d9d5b735e3851e8b48b9f9f2e17f79
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-04 02:14:24 +0900

    http関連の情報処理の操作をmainへ移管

--- client/client.go
+++ client/client.go
@@ -3,113 +3,57 @@ package client
 import (
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"path"
-	"regexp"
 	"strings"
 
-	"github.com/urfave/cli"
+	"../infomation"
 )
 
 //Client ...
 //clientはhttpリクエストを送信するためのモジュールです。
 type Client interface {
 	Request() (string, error)
-	GetImage()
 }
 
 type client struct {
-	URL      string
-	Method   string
-	FileName string
-	Data     url.Values
-}
-
-func (c *client) GetImage() {
-	response, err := http.Get(c.URL)
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-	if _, err := os.Stat(path.Dir(c.FileName)); os.IsNotExist(err) {
-		os.Mkdir(path.Dir(c.FileName), 0777)
-	}
-	file, err := os.Create(c.FileName)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	io.Copy(file, response.Body)
+	Info infomation.HttpInfomation
 }
 
 func (c *client) Request() (string, error) {
-	req, _ := http.NewRequest(c.Method, c.URL, strings.NewReader(c.Data.Encode()))
+	httpinfo := c.Info
+	req, _ := http.NewRequest(httpinfo.Method, httpinfo.URL, strings.NewReader(httpinfo.Data.Encode()))
+	if httpinfo.Method == "POST" {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
 
-	httpclient := new(http.Client)
+	httpclient := http.Client{}
 	resp, err := httpclient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
+	if httpinfo.Output.Flag {
+		if _, err := os.Stat(path.Dir(httpinfo.Output.Filename)); os.IsNotExist(err) {
+			os.Mkdir(path.Dir(httpinfo.Output.Filename), 0777)
+		}
+		file, err := os.Create(httpinfo.Output.Filename)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		io.Copy(file, resp.Body)
+		return "", nil
+	}
 	byteArray, _ := ioutil.ReadAll(resp.Body)
 	return string(byteArray), nil
 }
 
-func getUrl(c *cli.Context) string {
-	var urls []string
-	r := regexp.MustCompile(`^(http|https)://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$`)
-	for _, arg := range c.Args() {
-		if len(r.FindStringSubmatch(arg)) > 0 {
-			urls = append(urls, r.FindStringSubmatch(arg)[0])
-		}
-	}
-	return urls[len(urls)-1]
-}
-
-func New(c *cli.Context) Client {
-	var method string
-	var urlstring string
-	var filename string
-
-	urlstring = getUrl(c)
-
-	if c.Bool("O") {
-		u, err := url.Parse(urlstring)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if u.Path == "/" {
-			filename = "index.html"
-		} else {
-			filename = u.Path
-		}
-	} else if c.String("output") != "" {
-		filename = c.String("output")
-	} else {
-		filename = ""
-	}
-
-	if c.String("data") != "" {
-		method = "POST"
-	} else {
-		method = "GET"
-	}
-
-	values := url.Values{}
-
-	for _, data := range c.StringSlice("data") {
-		slice := strings.Split(data, "=")
-		values.Set(slice[0], slice[1])
-	}
+func New(h infomation.HttpInfomation) Client {
 	return &client{
-		URL:      urlstring,
-		Method:   method,
-		FileName: "." + filename,
-		Data:     values,
+		Info: h,
 	}
 }
