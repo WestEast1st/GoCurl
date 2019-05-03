a15a0bef6ac5dc7d22a433207744dfef30fdb136
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-03 20:55:13 +0900

    POSTへの対応

--- client/client.go
+++ client/client.go
@@ -3,6 +3,10 @@ package client
 import (
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/urfave/cli"
 )
 
 //Client ...
@@ -12,7 +16,10 @@ type Client interface {
 }
 
 type client struct {
-	URL string
+	URL      string
+	Method   string
+	FileName string
+	Data     string
 }
 
 func (c *client) Get() (string, error) {
@@ -29,6 +36,27 @@ func (c *client) Get() (string, error) {
 	return string(byteArray), nil
 }
 
-func New(url string) Client {
-	return &client{URL: url}
+func getUrl(c *cli.Context) string {
+	var urls []string
+	r := regexp.MustCompile(`^(http|https)://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$`)
+	for _, arg := range c.Args() {
+		if len(r.FindStringSubmatch(arg)) > 0 {
+			urls = append(urls, r.FindStringSubmatch(arg)[0])
+		}
+	}
+	return urls[len(urls)-1]
+}
+
+func New(c *cli.Context) Client {
+	var method string
+	if c.String("data") != "" {
+		method = "POST"
+	} else {
+		method = "GET"
+	}
+	return &client{
+		URL:    getUrl(c),
+		Method: method,
+		Data:   strings.Join(c.StringSlice("data"), "&"),
+	}
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 
 	"./client"
 	"github.com/urfave/cli"
@@ -17,21 +16,25 @@ func main() {
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:  "output, o",
-			Value: "./",
 			Usage: "保存名を指定してください",
 		},
-		cli.StringFlag{
+		cli.BoolTFlag{
+			Name:  "remote-name, O",
+			Usage: "リモートファイルのファイル部分を名前に利用します",
+		},
+		cli.StringSliceFlag{
 			Name:  "data, d",
 			Usage: "POST送信するデータを入力してください",
 		},
 	}
+
 	app.Action = func(c *cli.Context) error {
-		urls := getUrls(c)
-		client := client.New(urls[0])
+		client := client.New(c)
 		response, err := client.Get()
 		if err != nil {
 			return err
 		}
+		response = ""
 		fmt.Println(response)
 		return nil
 	}
@@ -39,15 +42,4 @@ func main() {
 	app.HideHelp = true
 
 	app.Run(os.Args)
-}
-
-func getUrls(c *cli.Context) []string {
-	var urls []string
-	r := regexp.MustCompile(`^(http|https)://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$`)
-	for _, arg := range c.Args() {
-		if len(r.FindStringSubmatch(arg)) > 0 {
-			urls = append(urls, r.FindStringSubmatch(arg)[0])
-		}
-	}
-	return urls
 }
